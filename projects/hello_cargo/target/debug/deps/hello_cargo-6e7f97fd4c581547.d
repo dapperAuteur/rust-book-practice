b8e98bf994287053127f7fa0f535f0afcb9a182c
dapperauteur <a@awews.com>
2018-08-08 02:00:23 -0400

    build project with cargo

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-6e7f97fd4c581547.d
@@ -0,0 +1,5 @@
+/Users/awe/proj/software/practice/rust/the-book/projects/hello_cargo/target/debug/deps/hello_cargo-6e7f97fd4c581547: src/main.rs
+
+/Users/awe/proj/software/practice/rust/the-book/projects/hello_cargo/target/debug/deps/hello_cargo-6e7f97fd4c581547.d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/awe/proj/software/practice/rust/the-book/projects/hello_cargo/target/debug/hello_cargo.dSYM: /Users/awe/proj/software/practice/rust/the-book/projects/hello_cargo/src/main.rs
